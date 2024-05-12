88a45c0229b5285fc3b2b63d2df8545170628428
Vijaya Malla <mallabiju@gmail.com>
2024-05-12 20:59:11 +0545

    JDBC

--- W_Applet/JavaApplet_Outdated/src/myapplet.java
+++ W_Applet/JavaApplet_Outdated/src/myapplet.java
@@ -1,9 +1,10 @@
-import java.applet.*;
+// import java.applet.*;
 import java.awt.*;
 
 // <applet code = "myapplet.class" width="200" height="100"></applet>
 
-public class myapplet extends Applet {
+// public class myapplet extends Applet {
+public class myapplet{
     public void paint(Graphics g) {
         g.drawLine(20,20,180,80); // line will be made
     }
